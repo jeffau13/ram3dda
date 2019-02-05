bfef83753770cf4537dd5556b127c73f586967af
jeffau13 <jeffau13@gmail.com>
2019-02-04 21:48:59 -0500

    testing disableing zoom and counter

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 //calls light Gallery and sets options
 $(document).ready(function() {
-  $("#imgwall").lightGallery({
+  //catch error
+  try {
+    $('#imgwall').lightGallery();
+    $('#imgwall')
+      .data('lightGallery')
+      .destroy(true);
+  } catch (ex) {}
+
+  $('#imgwall').lightGallery({
     thumbnail: true,
     showThumbByDefault: false,
     fullScreen: true,
     hideBarsDelay: 1500,
     mousewheel: false,
-    subHtmlSelectorRelative: true
+    subHtmlSelectorRelative: true,
+    counter: false,
+    animateThumb: false,
+    zoom: false
   });
 });
